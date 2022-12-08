f9927af650da5f8d3eb2207f0fda8c100a3848c6
Douglas Motta <douglas1102rm@hotmail.com>
2022-12-08 16:18:42 +0000

    Código inicial

--- app/src/main/java/br/com/rocketseatplus/jetpackcomposetutorial01/MainActivity.kt
+++ app/src/main/java/br/com/rocketseatplus/jetpackcomposetutorial01/MainActivity.kt
@@ -3,26 +3,41 @@ package br.com.rocketseatplus.jetpackcomposetutorial01
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import br.com.rocketseatplus.jetpackcomposetutorial01.ui.theme.JetpackComposeTutorial01Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(name = "Android")
+            JetpackComposeTutorial01Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
         }
     }
 }
 
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name")
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-fun PreviewMessageCard() {
-    MessageCard(name = "Android")
+fun DefaultPreview() {
+    JetpackComposeTutorial01Theme {
+        Greeting("Android")
+    }
 }
